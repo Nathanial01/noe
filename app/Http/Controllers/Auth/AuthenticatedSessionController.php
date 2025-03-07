7e73970e63d1ab0d31532e71d9f772a1e34004c5
Nathainal <nathainal@Nathainals-MacBook-Air.local>
2025-03-07 14:58:37 +0100

    Force rebuild after fixing buildpacks

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,64 +1,4 @@
 <?php
-//
-//namespace App\Http\Controllers\Auth;
-//
-//use App\Http\Controllers\Controller;
-//use App\Http\Requests\Auth\LoginRequest;
-//use Illuminate\Http\RedirectResponse;
-//use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Auth;
-//use Illuminate\Support\Facades\Route;
-//use Illuminate\Validation\ValidationException;
-//use Inertia\Inertia;
-//use Inertia\Response;
-//
-//class AuthenticatedSessionController extends Controller
-//{
-//    /**
-//     * Display the login view.
-//     */
-//    public function create(): Response
-//    {
-//        return Inertia::render('Auth/Login', [
-//            'canResetPassword' => Route::has('password.request'),
-//            'status' => session('status'),
-//        ]);
-//    }
-//
-//    /**
-//     * Handle an incoming authentication request.
-//     * @throws ValidationException
-//     */
-//    public function store(LoginRequest $request): RedirectResponse
-//    {
-//        $request->authenticate();
-//        $request->session()->regenerate();
-//
-//        // Get the authenticated user.
-//        $user = Auth::user();
-//
-//        // Determine the dashboard URL based on is_admin flag.
-//        $dashboardUrl = $user && $user->is_admin
-//            ? '/nova/dashboards/main'
-//            : '/nova/dashboards/clients';
-//
-//        return redirect()->intended($dashboardUrl);
-//    }
-//
-//    /**
-//     * Destroy an authenticated session.
-//     */
-//    public function destroy(Request $request): RedirectResponse
-//    {
-//        Auth::guard('web')->logout();
-//        $request->session()->invalidate();
-//        $request->session()->regenerateToken();
-//        return redirect('/');
-//    }
-//}
-//
-//
-
 
 namespace App\Http\Controllers\Auth;
 
@@ -87,6 +27,7 @@ class AuthenticatedSessionController extends Controller
 
     /**
      * Handle an incoming authentication request.
+     *
      * @throws ValidationException
      */
     public function store(LoginRequest $request): RedirectResponse
@@ -104,7 +45,7 @@ class AuthenticatedSessionController extends Controller
         // Get the authenticated user.
         $user = Auth::user();
 
-        // Determine the dashboard URL based on is_admin flag.
+        // Determine the dashboard URL based on the is_admin flag.
         $dashboardUrl = $user && $user->is_admin
             ? '/nova/dashboards/main'
             : '/nova/dashboards/clients';
